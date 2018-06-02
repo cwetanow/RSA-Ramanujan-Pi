1480d2c2574cafdbef681158eb74f9cdb01bed08
Ivan <madritistaaa@gmail.com>
2018-06-02 16:06:40 +0300

    Add initial parallel setup

---
+++ src/Project/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Project
+{
+	class Program
+	{
+		private static Stopwatch stopwatch = new Stopwatch();
+
+		static void Main(string[] args)
+		{
+			for (int threads = 1; threads < 9; threads++)
+			{
+				stopwatch.Restart();
+				Parallel.For(0, short.MaxValue * 10, new ParallelOptions { MaxDegreeOfParallelism = threads }, i =>
+				{
+					Factorial(i);
+				});
+
+				Console.WriteLine($"Time: {stopwatch.ElapsedMilliseconds}, Threads: {threads}");
+				stopwatch.Stop();
+			}
+		}
+
+		static void Factorial(int n)
+		{
+			var result = 1;
+
+			for (int i = 1; i < n; i++)
+			{
+				result *= i;
+			}
+		}
+	}
+}
