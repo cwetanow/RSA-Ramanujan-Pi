4e3e4b29a2d3b44a069c196115e50299115ecdab
Ivan <madritistaaa@gmail.com>
2018-06-08 19:25:15 +0300

    Extract parameters to file

---
+++ src/Project/Parameters.cs
@@ -0,0 +1,16 @@
+﻿namespace Project
+{
+    public class Parameters
+    {
+        public const string DefaultOutputFile = "result.txt";
+
+        // -p
+        public int ElementsCount { get; set; }
+        // -t
+        public int MaxTasks { get; set; }
+        // -q
+        public bool IsQuiet { get; set; }
+        // -o
+        public string OutputFile { get; set; }
+    }
+}
--- src/Project/Program.cs
+++ src/Project/Program.cs
@@ -5,20 +5,6 @@ using System.Threading.Tasks;
 
 namespace Project
 {
-    public class Parameters
-    {
-        public const string DefaultOutputFile = "result.txt";
-
-        // -p
-        public int ElementsCount { get; set; }
-        // -t
-        public int MaxTasks { get; set; }
-        // -q
-        public bool IsQuiet { get; set; }
-        // -o
-        public string OutputFile { get; set; }
-    }
-
     public class Program
     {
         private static readonly Stopwatch stopwatch = new Stopwatch();
