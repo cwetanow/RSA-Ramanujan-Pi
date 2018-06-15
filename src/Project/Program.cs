52846e66285eb8feeef23dba12d6ba17fd9004a9
Ivan <madritistaaa@gmail.com>
2018-06-15 09:07:28 +0300

    Extract parameters class in program file to copy only one file to server

--- src/Project/Program.cs
+++ src/Project/Program.cs
@@ -5,6 +5,20 @@ using System.Threading.Tasks;
 
 namespace Project
 {
+    public class Parameters
+    {
+        public const string DefaultOutputFile = "result.txt";
+
+        // -p
+        public int ElementsCount { get; set; }
+        // -t
+        public int MaxTasks { get; set; }
+        // -q
+        public bool IsQuiet { get; set; }
+        // -o
+        public string OutputFile { get; set; }
+    }
+
     public class Program
     {
         private static readonly Stopwatch stopwatch = new Stopwatch();
